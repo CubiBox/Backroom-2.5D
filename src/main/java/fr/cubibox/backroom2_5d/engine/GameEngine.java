96f26a21a3b4f604a3d9ff940ba2a9a325162369
Théo Duval <theo.duval27@outlook.fr>
2024-04-17 19:50:22 +0200

    Removing unused code

--- src/main/java/fr/cubibox/backroom2_5d/engine/GameEngine.java
+++ src/main/java/fr/cubibox/backroom2_5d/engine/GameEngine.java
@@ -5,14 +5,12 @@ import fr.cubibox.backroom2_5d.game.Backroom2D;
 public class GameEngine implements Runnable {
     private final Thread engineThread = new Thread(this, "ENGINE_THREAD");
 
-    public static final float ONE_SECOND_IN_NANO = 1E9F;
     public static final double ONE_SECOND_IN_MILLIS = 1E3;
 
     private static GameEngine instance = null;
 
     private final GameScene gameScene = new Backroom2D("map1.map");
 
-    private final long targetUps = 30L;
     private boolean running = false;
 
     private GameEngine() {
@@ -37,11 +35,11 @@ public class GameEngine implements Runnable {
     @Override
     public void run() {
         long startTime = System.currentTimeMillis();
+        long targetUps = 30L;
         long targetUpdateTime = (long) (ONE_SECOND_IN_MILLIS / targetUps);
         long updateTime = startTime;
 
         float time = 0;
-        int update = 0;
 
         while (running) {
             long now = System.currentTimeMillis();
@@ -50,7 +48,6 @@ public class GameEngine implements Runnable {
             gameScene.update(dt);
 
             if (time >= 1) {
-                update = 0;
                 time--;
             }
 
@@ -59,10 +56,9 @@ public class GameEngine implements Runnable {
                 float sleepTime = (now + targetUpdateTime) - currentTime;
 
                 time += dt;
-                update++;
 
                 if (sleepTime > 0) {
-                    Thread.sleep((long) sleepTime);
+                    wait((long) sleepTime);
                 }
             } catch (Exception e) {
                 throw new RuntimeException();
